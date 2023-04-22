c87c2f066092028afb0307883a23d6fe67361ac6
Marcos Miguel Avelino <56806907+marcoosavelino@users.noreply.github.com>
2023-04-22 19:23:17 +0000

    Correção

--- AutoFix/Program.cs
+++ AutoFix/Program.cs
@@ -22,7 +22,7 @@ internal class Program
 
         var app = builder.Build();
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
             app.UseHsts();
